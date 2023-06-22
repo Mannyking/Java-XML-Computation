d1d35b31bb73a2152ea437628e16303b0d38574b
Mannyking <68853328+Mannyking@users.noreply.github.com>
2023-06-22 13:07:58 +0100

    Environment Setup
    - JavaFX is being used for the development of the application
    - SceneBuilder configured

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.jfx.xmloutput.xmloutputfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.jfx.xmloutput.xmloutputfx to javafx.fxml;
+    exports com.jfx.xmloutput.xmloutputfx;
+}
